fd38ce4732a6527043a262d189ac725581b61e74
Alex <alex@sancocholabs.tech>
2020-04-02 14:20:15 -0400

    Rendering components in MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'My Gatsby Site',
+    description: 'A site built using Gatsby.',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
